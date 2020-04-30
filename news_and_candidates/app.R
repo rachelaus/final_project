eae1c6036f252b3929f633abe58c576400cce3a4
Rachel Auslander <rachelauslander@college.harvard.edu>
2020-04-30 13:43:42 -0400

    commit

--- news_and_candidates/app.R
+++ news_and_candidates/app.R
@@ -1,104 +1,128 @@
+# imported libraries 
 
+library(tidyverse)
 library(shiny)
 library(shinythemes)
+library(ggplot2)
+library(ggthemes)
+
+
+# imported data sources
 
 joined_data <- read_rds("joined_data.rds")
 viewbyage <- read_rds("viewbyage.rds")
 viewbyrace <- read_rds("viewbyrace.rds")
 
-# Define UI for application that draws a histogram
+# created a vector of news sources to be used in the checkbox picker
+
+news_sources_vector = c("AM Radio", "CNN", "Facebook", "Fox", "Local News", "Local TV", 
+           "MSNBC", "Network TV", "New York Times", "NPR", "Telemundo")
+
+# UI of Shiny app
+
 ui <- fluidPage(
-    
+  
+  # added a shiny theme
+  
   theme = shinytheme("flatly"),
   
   # Title describes the overarching question of my project
-  navbarPage("Investigating bias of mainstream media coverage and readers",
+  navbarPage("Investigating readers of mainstream media coverage and potential bias",
              
-             # I created a "Visualizations" page to show different candidates' ad strategies
-             # as a first pass at finding a relationship between ad spend and poll results
+          
+  # investigating the presidential candidate headlines by different sources
              
-             tabPanel("Comparison of Candidate Coverage",
-                      tabsetPanel(
+  tabPanel("Comparison of Presidential Candidate Coverage",
+     tabsetPanel(
                         
-                        # The "Overview" panel looks at the relationship between ad spend 
-                        # and polling
-                        
-                        tabPanel("Joe vs. Bernie",
+          # New York Times headlines
+          
+          tabPanel("New York Times",
 
+              # Sidebar with a slider input for the number of bins
+                sidebarPanel(
+                  sliderInput("bin",
+                             "Date of article:",
+                              min = as.Date("2020-01-01","%Y-%m-%d"),
+                              max = as.Date("2020-04-17","%Y-%m-%d"),
+                              value=as.Date("2020-02-16"),
+                              timeFormat="%Y-%m-%d")
+                ),
+                
+               # displayed comparison of headlines
+              
+                mainPanel(
+                  tableOutput("headlineComparison")
+                  )
+              ) # end new york times
+    ) # end tabset panel
+  ), # end tab panel
     
-                        # Sidebar with a slider input for the number of bins
-                          sidebarPanel(
-                            sliderInput("bin",
-                                       "Date of article:",
-                                        min = as.Date("2020-01-01","%Y-%m-%d"),
-                                        max = as.Date("2020-04-17","%Y-%m-%d"),
-                                        value=as.Date("2020-02-16"),
-                                        timeFormat="%Y-%m-%d")
-                          ),
-                          
-                          mainPanel(
-                            tableOutput("tweetComparison")
-                            )
-                          
-        
-      ) # end tabset panel
-    ) # end tab panel
     
+    # investigating who reads different sources
     
-    
-    ), # end navbar
-    
-    tabPanel("Comparison of Candidate Coverage",
-             tabsetPanel(
-               
-               # The "Overview" panel looks at the relationship between ad spend 
-               # and polling
-               
+    tabPanel("Readership",
+             
+          tabsetPanel(
+             
                tabPanel("Age",
              
-       h3("Relationship between Ad Spending & Polling Results by Candidate"),       
+       h3("News sources popular among different age demographics"),       
        mainPanel(plotOutput("viewbyage"))
-               ),
-       
+               ), 
+       sidebarPanel(
+                 checkboxGroupInput("news_checkbox","Select News Sources",
+                                    choices = news_sources_vector,
+                                    selected = news_sources_vector)),
+          
           tabPanel("Race",
-                   mainPanel(plotOutput("viewbyrace")))
-       
-        )
-    )
+                   h3("News sources popular among different racial demographics"),       
+                   mainPanel(
+                   plotOutput("viewbyrace"))
+                   
+                   )
+                   )
     
-) # end fluid page
+    ))
+    
+
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-    output$tweetComparison <- renderTable({
-      joined_data %>% filter(pub_date == input$bin) %>% select(headline.main)
-    }, colnames = FALSE)
- 
-    output$age_plot <- renderPlot({
-      viewbyage %>%
-        ggplot(aes(x = news_source, y = num_news, fill = age_range)) + 
-        geom_col() + 
-        labs(title = "Ages of readers of each news source", fill = "Age Range", x = "News Source", y = "Number of Readers/Viewers") + theme_classic() + theme(axis.text.x=element_text(angle=45,hjust=1)) +
-        
-        # renamed x axis labels in order, not the most efficient way but ok for now
-        
-        scale_x_discrete(labels = c("AM Radio", "CNN", "Facebook", "Fox", "Local News", "Local TV", "MSNBC", "Network TV", "New York Times", "NPR", "Telemundo"))
-    })
+  
+  output$headlineComparison <- renderTable({
+    joined_data %>% filter(pub_date == input$bin) %>% select(headline.main)
+  }, colnames = FALSE)
+  
+  output$age_plot <- renderPlot({
+    viewbyage %>%
+    filter(news_source %in% input$news_checkbox) %>%
+    ggplot(aes(x = age_range, y = percent, fill = news_source)) + 
+      geom_col() + 
+      labs(title = "Ages of readers of each news source", 
+           fill = "News Source", 
+           x = "Age Range", y = "Percent of Readers/Viewers") + 
+      theme_classic() + theme(axis.text.x=element_text(angle=45,hjust=1))
+  })
+  
+  output$race_plot <- renderPlot({
+    viewbyrace %>%
+      ggplot(aes(x = news_source, y = num_news, fill = race_ethnicity)) + 
+      geom_col() + 
+      labs(title = "Races of readers of each news source", 
+           fill = "Race", 
+           x = "News Source", 
+           y = "Number of Readers/Viewers") + 
+      theme_classic() + 
+      theme(axis.text.x=element_text(angle=45,hjust=1)) + 
+      scale_x_discrete(labels = c("AM Radio", "CNN", "Facebook", "Fox", 
+                                  "Local News", "Local TV", "MSNBC", 
+                                  "Network TV", "New York Times", "NPR", 
+                                  "Telemundo"))
+  })
     
-    output$race_plot <- renderPlot({
-      viewbyrace %>%
-        ggplot(aes(x = news_source, y = num_news, fill = race_ethnicity)) + 
-        geom_col() + 
-        labs(title = "Races of readers of each news source", 
-             fill = "Race", 
-             x = "News Source", 
-             y = "Number of Readers/Viewers") + 
-        theme_classic() + 
-        theme(axis.text.x=element_text(angle=45,hjust=1)) + 
-        scale_x_discrete(labels = c("AM Radio", "CNN", "Facebook", "Fox", "Local News", "Local TV", "MSNBC", "Network TV", "New York Times", "NPR", "Telemundo"))
-    })
     
 }
 
