5e8baa604681f387bdba22f7a19c547d8346cf0e
Rachel Auslander <rachelauslander@college.harvard.edu>
2020-05-08 22:25:27 -0400

    updated

--- cleaning_data.R
+++ cleaning_data.R
@@ -24,6 +24,13 @@ colnames(joe_data) <- str_replace(colnames(joe_data),
 
 joe_data <- joe_data %>% mutate(candidate = "Joe Biden") %>% select(web_url, snippet, lead_paragraph, source, pub_date, type_of_material, headline.main, headline.print_headline, candidate) 
 
+joined_data_plot <- bernie_data %>% full_join(joe_data)
+
+
+# converting dates/times of joined data to just dates
+
+joined_data_plot$pub_date <- as.Date(substr(joined_data_plot$pub_date,1,10))
+
 # joining datasets
 
 joined_data <- bernie_data %>% full_join(joe_data)
@@ -36,7 +43,19 @@ joined_data$pub_date <- as.Date(substr(joined_data$pub_date,1,10))
 
 joined_data <- joined_data %>% distinct(headline.main, .keep_all = TRUE)
 
-
 # save joined data as data file
 
 saveRDS(joined_data,file="joined_data.rds")
+
+sort_by_date <- joined_data_plot %>%
+  arrange(pub_date) %>%
+  group_by(pub_date, candidate) %>%
+  select(pub_date, candidate) %>%
+  mutate(articles_per_date_bernie = sum(candidate == "Bernie Sanders")) %>%
+  mutate(articles_per_date_joe = sum(candidate == "Joe Biden")) %>%
+  distinct() %>%
+  pivot_longer(cols = starts_with("articles_per_date"),names_to = "candidate2", values_to = "articles_per_date") %>%
+  filter(articles_per_date != 0) %>%
+  select(-candidate2)
+
+saveRDS(sort_by_date,file="sort_by_date.rds")
--- cleaning_demographic_data.R
+++ cleaning_demographic_data.R
@@ -46,11 +46,28 @@ viewbyage <- dec26 %>%
   # a single column
   
   pivot_longer(-age_range, names_to = "news_source", values_to = "num_news")  %>%
-  group_by(age_range)
+  group_by(age_range) %>%
+  mutate(percent = (num_news/sum(num_news))*100) %>%
+  arrange(news_source) %>%
+  mutate(news_source = fct_recode(news_source, 
+                                  "New York Times" = "new_york_times",
+                                  "AM Radio" = "am_radio",
+                                  "CNN" = "cnn",
+                                  "Facebook" = "fb",
+                                  "Fox" = "fox",
+                                  "Local News" = "local_news",
+                                  "Local TV" = "local_tv",
+                                  "MSNBC" = "msnbc",
+                                  "Network TV" = "network_tv",
+                                  "NPR" = "npr",
+                                  "Telemundo" = "telemundo"
+                                  
+  )) 
 
 saveRDS(viewbyage,file="viewbyage.rds")
 
 # viewing by race
+
 
 viewbyrace <- dec26 %>%
   select(race_ethnicity, news_sources_msnbc, news_sources_cnn, news_sources_facebook, news_sources_fox, news_sources_network, 
@@ -67,6 +84,58 @@ viewbyrace <- dec26 %>%
             npr = sum(news_sources_npr == 1), 
             am_radio = sum(news_sources_amtalk == 1), 
             local_news = sum(news_sources_local_newspaper == 1)
-  ) %>% pivot_longer(-race_ethnicity, names_to = "news_source", values_to = "num_news") %>% as_factor(levels = "labels")
+  ) %>% pivot_longer(-race_ethnicity, names_to = "news_source", values_to = "num_news") %>% as_factor(levels = "labels") %>%
+  
+  # i was recoding in the wrong spot. i moved it to the right spot! lol
+  
+  mutate(race_ethnicity = fct_recode(race_ethnicity, 
+                                     "Asian American + Pacific Islander" = "Asian (Filipino)", 
+                                     "Asian American + Pacific Islander" = "Asian (Japanese)",
+                                     "Asian American + Pacific Islander" = "Asian (Chinese)",
+                                     "Asian American + Pacific Islander" = "Asian (Asian Indian)",
+                                     "Asian American + Pacific Islander" = "Asian (Korean)",
+                                     "Asian American + Pacific Islander" = "Asian (Vietnamese)",
+                                     "Asian American + Pacific Islander" = "Asian (Other)",
+                                     "Asian American + Pacific Islander" = "Pacific Islander (Native Hawaiian)",
+                                     "Asian American + Pacific Islander" = "Pacific Islander (Guamanian)",
+                                     "Asian American + Pacific Islander" = "Pacific Islander (Samoan)",
+                                     "Asian American + Pacific Islander" = "Pacific Islander (Other)"
+  )) %>%
+  group_by(race_ethnicity, news_source) %>%
+  summarize(num_news = sum(num_news)) %>%
+  mutate(percent = (num_news/sum(num_news))*100) %>%
+  arrange(race_ethnicity) %>%
+  mutate(news_source = fct_recode(news_source, 
+                                  "New York Times" = "new_york_times",
+                                  "AM Radio" = "am_radio",
+                                  "CNN" = "cnn",
+                                  "Facebook" = "fb",
+                                  "Fox" = "fox",
+                                  "Local News" = "local_news",
+                                  "Local TV" = "local_tv",
+                                  "MSNBC" = "msnbc",
+                                  "Network TV" = "network_tv",
+                                  "NPR" = "npr",
+                                  "Telemundo" = "telemundo"
+                                  
+  )) 
 
 saveRDS(viewbyrace, file="viewbyrace.rds")
+
+
+viewbyparty <- dec26 %>%
+  select(primary_party, dem_vote_intent, rank_dems_1, rank_dems_2, rank_dems_3, house_intent, news_sources_cnn, news_sources_fox, news_sources_npr) %>%
+  # filter(dem_vote_intent != 888) %>%
+  # filter(rank_dems_1 != 888) %>%
+  # filter(rank_dems_2 != 888) %>%
+  # filter(rank_dems_3 != 888)
+  mutate(party =
+           case_when(
+             primary_party == 1 ~ "Democrat",
+             primary_party == 2 ~ "Republican"
+           )) %>%
+  arrange(party) %>%
+  group_by(party)
+
+
+saveRDS(viewbyparty, file = "viewbyparty.rds")
